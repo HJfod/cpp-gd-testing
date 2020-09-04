75a2378d927dea018eeaf5be0f4573ad713cf25b
hjfod <dreadrollmusic@gmail.com>
2020-09-04 14:58:15 +0300

    i have developed a deep and passionate hatred for c++ due to gzip

--- ginfo.cpp
+++ ginfo.cpp
@@ -6,10 +6,18 @@
 #include <Combaseapi.h>
 #include <fstream>
 #include <vector>
+#include "gzip/zlib.h"
+#include <assert.h>
 
-#include "boost/iostreams/filtering_streambuf.hpp"
-#include "boost/iostreams/copy.hpp"
-#include "boost/iostreams/filter/gzip.hpp"
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
+
+#define CHUNK 16384
 
 // clang ginfo.cpp -o gd.exe -lshell32 -lole32
 
@@ -86,18 +94,48 @@ string DECODE_BASE64(const string &in) {
     return out;
 }
 
-string DECOMPRESS_GZIP(const string& DATA) {
-    using namespace boost::iostreams::zlib;
-    using namespace boost::iostreams;
+std::string DECOMPRESS_GZIP(const string& str) {
+    #define MOD_GZIP_ZLIB_WINDOWSIZE 15
+    #define MOD_GZIP_ZLIB_CFACTOR    9
+    #define MOD_GZIP_ZLIB_BSIZE      8096
 
-    stringstream RES;
-    
-    filtering_streambuf<input> in;
-    in.push(gzip_decompressor());
-    in.push(STRBYTE(DATA));
-    boost::iostreams::copy(in, RES);
-    
-    return RES.str();
+    z_stream zs;                        // z_stream is zlib's control structure
+    memset(&zs, 0, sizeof(zs));
+
+    if (inflateInit2(&zs, MOD_GZIP_ZLIB_WINDOWSIZE + 16) != Z_OK)
+        throw(std::runtime_error("inflateInit failed while decompressing."));
+
+    zs.next_in = (Bytef*)str.data();
+    zs.avail_in = str.size();
+
+    int ret;
+    char outbuffer[32768];
+    std::string outstring;
+
+    // get the decompressed bytes blockwise using repeated calls to inflate
+    do {
+        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+        zs.avail_out = sizeof(outbuffer);
+
+        ret = inflate(&zs, 0);
+
+        if (outstring.size() < zs.total_out) {
+            outstring.append(outbuffer,
+                             zs.total_out - outstring.size());
+        }
+
+    } while (ret == Z_OK);
+
+    inflateEnd(&zs);
+
+    if (ret != Z_STREAM_END) {          // an error occurred that was not EOF
+        std::ostringstream oss;
+        oss << "Exception during zlib decompression: (" << ret << ") "
+            << zs.msg;
+        throw(std::runtime_error(oss.str()));
+    }
+
+    return outstring;
 }
 
 int main(int ARGC, char *ARGS[]) {
