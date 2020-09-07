8d1893af12f1fb1d7690ad72e210586c4c1f62a1
HJfod <60038575+HJfod@users.noreply.github.com>
2020-09-07 10:43:36 +0300

    added GET_PROP

--- ginfo.cpp
+++ ginfo.cpp
@@ -24,7 +24,9 @@
 
 #define APP_VERSION "v1.0"
 
-// RUN COMMAND: clang ginfo.cpp ext/ZlibHelper.cpp -o gd.exe -L"." -lshell32 -lole32 -lzlib -m32 -std=c++17 -O3; ./gd.exe moi
+// BUILD COMMAND: clang ginfo.cpp ext/ZlibHelper.cpp -o gd.exe -L"." -lshell32 -lole32 -lzlib -m32 -std=c++17 -O3
+
+// RUN COMMAND: ./ginfo <level_name> <level_name> <level_name> ...
 
 std::string REPL( std::string const& original, std::string const& from, std::string const& to ) {
     std::string results;
@@ -86,28 +88,8 @@ std::string DECOMPRESS_GZIP(const std::vector<uint8_t> str) {
     return std::string(buffer.data(), buffer.data() + buffer.size());
 }
 
-std::string GET_KEY(const std::string DATA, std::string KEY, std::string TYPE = ".*?") {
-    if (TYPE == "") {
-        std::regex m ("<k>" + KEY + "</k>");
-        return (std::regex_search(DATA, m)) ? "True" : "False";
-    } else {
-        std::regex m ("<k>" + KEY + "</k><" + TYPE + ">");
-        std::smatch cm;
-        std::regex_search(DATA, cm, m);
-
-        if (cm[0] == "") return "";
-
-        std::string T_TYPE = ((std::string)cm[0]).substr(((std::string)cm[0]).find_last_of("<") + 1, 1);
-
-        std::regex tm ("<k>" + KEY + "</k><" + T_TYPE + ">.*?</" + T_TYPE + ">");
-        std::smatch tcm;
-        std::regex_search(DATA, tcm, tm);
-
-        std::string VAL = tcm[0];
-
-        int L1 = ("<k>" + KEY + "</k><" + T_TYPE + ">").length();
-        return VAL.substr(L1, VAL.find_last_of("</") - L1 - 1);
-    }
+constexpr unsigned int HASH(const char* str, int h = 0) {
+    return !str[h] ? 5381 : (HASH(str, h+1) * 33) ^ str[h];
 }
 
 std::vector<std::string> GET_LEVELS(const std::string DATA) {
@@ -168,6 +150,97 @@ std::string REPL_LENGTH(int L) {
     return L >= 0 && L <= 4 ? LGTS[L] : "Unknown";
 }
 
+std::string GET_KEY(const std::string DATA, std::string KEY, std::string TYPE = ".*?") {
+    if (TYPE == "") {
+        std::regex m ("<k>" + KEY + "</k>");
+        return (std::regex_search(DATA, m)) ? "True" : "False";
+    } else {
+        std::regex m ("<k>" + KEY + "</k><" + TYPE + ">");
+        std::smatch cm;
+        std::regex_search(DATA, cm, m);
+
+        if (cm[0] == "") return "";
+
+        std::string T_TYPE = ((std::string)cm[0]).substr(((std::string)cm[0]).find_last_of("<") + 1, 1);
+
+        std::regex tm ("<k>" + KEY + "</k><" + T_TYPE + ">.*?</" + T_TYPE + ">");
+        std::smatch tcm;
+        std::regex_search(DATA, tcm, tm);
+
+        std::string VAL = tcm[0];
+
+        int L1 = ("<k>" + KEY + "</k><" + T_TYPE + ">").length();
+        return VAL.substr(L1, VAL.find_last_of("</") - L1 - 1);
+    }
+}
+
+std::string GET_PROP(const std::string DATA, std::string PROP) {
+    // basically an abstracted shorthand for GET_KEY
+
+    std::string RES;
+    std::string WORKSTR;
+    std::vector<uint8_t> WORKVCT;
+    long WORKINT;
+
+    switch (HASH(PROP.c_str())) {
+        case HASH("NAME"):
+            RES = GET_KEY(DATA, "k2");
+            break;
+        case HASH("CREATOR"):
+            RES = GET_KEY(DATA, "k5");
+            break;
+        case HASH("DATA"):
+            RES = GET_KEY(DATA, "k4");
+            break;
+        case HASH("LENGTH"):
+            WORKSTR = GET_KEY(DATA, "k23");
+            RES = (WORKSTR == "" ? "Tiny" : REPL_LENGTH(std::stoi(WORKSTR)));
+            break;
+        case HASH("VERSION"):
+            RES = GET_KEY(DATA, "k16");
+            break;
+        case HASH("PASSWORD"):
+            WORKSTR = GET_KEY(DATA, "k41");
+            RES = (WORKSTR == "1" || WORKSTR == "" ? "Free to Copy" : WORKSTR == "0" ? "No Copy" : WORKSTR);
+            break;
+        case HASH("SONG"):
+            WORKSTR = GET_KEY(DATA, "k8");
+            RES = (WORKSTR != "" ? REPL_O_SONG(std::stoi(WORKSTR)) : GET_KEY(DATA, "k45") == "" ? "Unknown" : GET_KEY(DATA, "k45"));
+            break;
+        case HASH("DESCRIPTION"):
+            WORKVCT = DECODE_BASE64(GET_KEY(DATA, "k3"));
+            WORKSTR = std::string(WORKVCT.data(), WORKVCT.data() + WORKVCT.size());
+            RES = (WORKSTR == "" ? "None" : WORKSTR);
+            break;
+        case HASH("OBJECT COUNT"):
+            RES = GET_KEY(DATA, "k48");
+            break;
+        case HASH("EDITOR TIME"):
+            WORKINT = std::stol(GET_KEY(DATA, "k80"));
+            RES = (WORKINT > 3600 ? std::to_string((int)(WORKINT / 3600.0)) + "h" : std::to_string((int)(WORKINT / 60.0)) + "m" );
+            break;
+        case HASH("VERIFIED"):
+            RES = GET_KEY(DATA, "k14", "");
+            break;
+        case HASH("ATTEMPTS"):
+            WORKSTR = GET_KEY(DATA, "k18");
+            RES = (WORKSTR == "" ? "None" : WORKSTR);
+            break;
+        case HASH("REVISION"):
+            WORKSTR = GET_KEY(DATA, "k46");
+            RES = (WORKSTR == "" ? "None" : WORKSTR);
+            break;
+        case HASH("COPY"):
+            WORKSTR = GET_KEY(DATA, "k42");
+            RES = (WORKSTR == "" ? "None" : WORKSTR);
+            break;
+        default:
+            RES = "Unknown property.";
+    }
+
+    return RES;
+}
+
 int main(int ARGC, char *ARGS[]) {
     if (ARGC > 1) {
         std::cout << "GD Level Info Tool " << APP_VERSION << std::endl << std::endl;
@@ -207,29 +280,19 @@ int main(int ARGC, char *ARGS[]) {
             if (LVL == "") {
                 std::cout << std::endl << "Could not find level! (Replace spaces in name with _)" << std::endl;
             } else {
-                std::string LGT = GET_KEY(LVL, "k23");
-                std::string PW = GET_KEY(LVL, "k41");
-                std::string SONG = GET_KEY(LVL, "k8");
-                std::vector<uint8_t> D_BUFF = DECODE_BASE64(GET_KEY(LVL, "k3"));
-                std::string DESC = std::string(D_BUFF.data(), D_BUFF.data() + D_BUFF.size());
-                std::string REV = GET_KEY(LVL, "k46");
-                std::string COPY = GET_KEY(LVL, "k42");
-                std::string ATT = GET_KEY(LVL, "k18");
-                long E_TIME = std::stol(GET_KEY(LVL, "k80"));
-
-                std::cout << std::endl << "Name: \t\t" <<          GET_KEY(LVL, "k2");
-                std::cout << std::endl << "Creator: \t" <<       GET_KEY(LVL, "k5");
-                std::cout << std::endl << "Length: \t" <<        (LGT == "" ? "Tiny" : REPL_LENGTH(std::stoi(LGT)));
-                std::cout << std::endl << "Version: \t" <<       GET_KEY(LVL, "k16");
-                std::cout << std::endl << "Password: \t" <<      (PW == "1" || PW == "" ? "Free to Copy" : PW == "0" ? "No Copy" : PW);
-                std::cout << std::endl << "Song: \t\t" <<        (SONG != "" ? REPL_O_SONG(std::stoi(SONG)) : GET_KEY(LVL, "k45") == "" ? "Unknown" : GET_KEY(LVL, "k45"));
-                std::cout << std::endl << "Description: \t" <<   (DESC == "" ? "None" : DESC);
-                std::cout << std::endl << "Object count: \t" <<  GET_KEY(LVL, "k48");
-                std::cout << std::endl << "Editor time: \t" <<   (E_TIME > 3600 ? std::to_string((int)(E_TIME / 3600.0)) + "h" : std::to_string((int)(E_TIME / 60.0)) + "m" );
-                std::cout << std::endl << "Verified: \t" <<      GET_KEY(LVL, "k14", "");
-                std::cout << std::endl << "Attempts: \t" <<      (ATT == "" ? "None" : ATT);
-                std::cout << std::endl << "Revision: \t" <<      (REV == "" ? "None" : REV);
-                std::cout << std::endl << "Copied from: \t" <<   (COPY == "" ? "None" : COPY);
+                std::cout << std::endl << "Name: \t\t" <<        GET_PROP(LVL, "NAME");
+                std::cout << std::endl << "Creator: \t" <<       GET_PROP(LVL, "CREATOR");
+                std::cout << std::endl << "Length: \t" <<        GET_PROP(LVL, "LENGTH");
+                std::cout << std::endl << "Version: \t" <<       GET_PROP(LVL, "VERSION");
+                std::cout << std::endl << "Password: \t" <<      GET_PROP(LVL, "PASSWORD");
+                std::cout << std::endl << "Song: \t\t" <<        GET_PROP(LVL, "SONG");
+                std::cout << std::endl << "Description: \t" <<   GET_PROP(LVL, "DESCRIPTION");
+                std::cout << std::endl << "Object count: \t" <<  GET_PROP(LVL, "OBJECT COUNT");
+                std::cout << std::endl << "Editor time: \t" <<   GET_PROP(LVL, "EDITOR TIME");
+                std::cout << std::endl << "Verified: \t" <<      GET_PROP(LVL, "VERIFIED");
+                std::cout << std::endl << "Attempts: \t" <<      GET_PROP(LVL, "ATTEMPTS");
+                std::cout << std::endl << "Revision: \t" <<      GET_PROP(LVL, "REVISION");
+                std::cout << std::endl << "Copied from: \t" <<   GET_PROP(LVL, "COPY");
             }
 
             std::cout << std::endl << std::endl;
